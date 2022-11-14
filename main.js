978ea1f417bd7b524a1fadc3a7c74ef27ab4ece7
J Luis <vargasibarra01@gmail.com>
2022-11-14 12:44:04 -0600

    Modificado; no se puede redimencionar la ventana

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 550,
+    height: 220,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    resizable: false
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('ventanas/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
